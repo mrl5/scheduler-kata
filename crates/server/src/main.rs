0105b55ec5985902f6e62e52c5f539e4e3e9a1be
mrl5 <31549762+mrl5@users.noreply.github.com>
2022-10-07 16:39:49 +0200

    refactor: little clean-up

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,27 +1,21 @@
 use axum::{response::Json, routing::get, Router, Server};
-use hyper::Response;
 use serde::Serialize;
 use tower::ServiceBuilder;
-use tower_http::trace::{MakeSpan, OnResponse, TraceLayer};
-use tracing::{field, Level, Span};
+use tower_http::trace::TraceLayer;
+use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
 
 const ADDR: &str = "0.0.0.0:3000";
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     let subscriber = FmtSubscriber::builder()
         .with_max_level(Level::DEBUG)
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
     tracing::debug!("tracing initiated");
-    let middleware_stack = ServiceBuilder::new().layer(
-        TraceLayer::new_for_http()
-            .on_response(MyOnResponse {})
-            .make_span_with(MyMakeSpan {})
-            .on_request(()),
-    );
+    let middleware_stack = ServiceBuilder::new().layer(TraceLayer::new_for_http().on_request(()));
     // note: issues with setting up the logs
     // update: I was probably confused with my own tests ... :/
 
@@ -30,9 +24,11 @@ async fn main() {
         .layer(middleware_stack);
 
     tracing::info!("Starting server ...");
-    let server = Server::bind(&ADDR.parse().unwrap()).serve(app.into_make_service());
+    let server = Server::bind(&ADDR.parse()?).serve(app.into_make_service());
+
     println!("Server running at {}", ADDR);
-    server.await.unwrap();
+    futures::try_join!(server)?;
+    Ok(())
 }
 
 #[derive(Serialize)]
@@ -50,38 +46,4 @@ async fn run_healthcheck() -> Json<Health> {
         status: HealthStatus::Healthy,
     };
     Json(check)
-}
-
-#[derive(Clone)]
-pub struct MyOnResponse {}
-
-impl<B> OnResponse<B> for MyOnResponse {
-    fn on_response(
-        self,
-        response: &Response<B>,
-        latency: std::time::Duration,
-        _span: &tracing::Span,
-    ) {
-        tracing::info!(
-            latency = latency.as_millis(),
-            status = response.status().as_u16(),
-            "response"
-        )
-    }
-}
-
-#[derive(Clone)]
-pub struct MyMakeSpan {}
-
-impl<B> MakeSpan<B> for MyMakeSpan {
-    fn make_span(&mut self, request: &hyper::Request<B>) -> Span {
-        tracing::info_span!(
-            "request",
-            method = %request.method(),
-            uri = %request.uri(),
-            username = field::Empty,
-            workspace_id = field::Empty,
-            email = field::Empty,
-        )
-    }
 }
--- crates/workers/src/lib.rs
+++ crates/workers/src/lib.rs
@@ -1,1 +1,74 @@
+use std::collections::VecDeque;
+use std::thread::sleep;
+use std::time::Duration;
+use tasks::{Task, TaskType};
+use time::OffsetDateTime;
+use tokio::spawn;
+use tokio::sync::mpsc::{self, UnboundedSender};
+use tokio::task::spawn_blocking;
 
+pub async fn run_workers() -> anyhow::Result<()> {
+    let (tx, mut rx) = mpsc::unbounded_channel::<Task>();
+    let mut pending_queue = get_tasks();
+    let mut processed_buffer = Vec::<Task>::new();
+
+    spawn(async move {
+        let mut interval = tokio::time::interval(Duration::from_millis(10));
+
+        loop {
+            interval.tick().await;
+
+            // todo: propose to discuss potential issue with this solution
+            while let Some(task) = pending_queue.pop_front() {
+                if !task.can_be_started() {
+                    pending_queue.push_back(task);
+                } else {
+                    let ctx = tx.clone();
+                    // note: issues when `some_computation` is async
+                    // self note: if it'd be async does it even make sense to run spawn_blocking?
+                    // IMO no
+                    spawn_blocking(move || some_computation(task, ctx));
+                }
+            }
+        }
+    });
+
+    while let Some(res) = rx.recv().await {
+        processed_buffer.push(res);
+    }
+
+    Ok(())
+}
+
+fn some_computation(mut task: Task, tx: UnboundedSender<Task>) -> anyhow::Result<()> {
+    let block_period = Duration::from_secs(5);
+    let id = task.id.to_string();
+
+    let res = task.set_as_running();
+    match res {
+        Ok(_) => tracing::info!("started task {}", id),
+        Err(_) => tracing::error!("failed to run task {}", id),
+    }
+
+    sleep(block_period);
+    match task.set_as_completed() {
+        Ok(_) => tracing::info!("completed task {}", id),
+        Err(_) => tracing::error!("unable to mark task {} as completed", id),
+    };
+
+    Ok(tx.send(task)?)
+}
+
+// todo: delete
+fn get_tasks() -> VecDeque<Task> {
+    let mut tasks = VecDeque::<Task>::new();
+    let t1 = Task::new(TaskType::TypeA, OffsetDateTime::now_utc());
+    let t2 = Task::new(TaskType::TypeB, OffsetDateTime::now_utc());
+    let t3 = Task::new(TaskType::TypeC, OffsetDateTime::now_utc());
+
+    tasks.push_back(t1);
+    tasks.push_back(t2);
+    tasks.push_back(t3);
+
+    tasks
+}
